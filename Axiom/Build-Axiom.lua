2e7076dcbe914e665046a19679d44550bc692cd3
Pedro Bussab <pedromonas22@gmail.com>
2025-07-01 19:22:29 -0300

    Start o fix to the GLFW submodule

--- Axiom/Build-Axiom.lua
+++ Axiom/Build-Axiom.lua
@@ -1,5 +1,52 @@
--- Build-Axiom.lua (add at top, before your project { … })
-local VulkanURL = "https://sdk.lunarg.com/sdk/download/latest/windows/VulkanSDK.exe"
+local VulkanLatestURL = "https://vulkan.lunarg.com/sdk/latest/windows.txt"
+local VulkanSDKBaseURL = "https://sdk.lunarg.com/sdk/download/"
+local VulkanSDKDir = "vendor/VulkanSDK"
+
+-- Helper to run shell commands and capture output
+local function os_capture(cmd)
+    local f = assert(io.popen(cmd, 'r'))
+    local s = assert(f:read('*a'))
+    f:close()
+    return s
+end
+
+local function get_latest_vulkan_version()
+    local version = os_capture('curl -s "' .. VulkanLatestURL .. '"'):gsub("%s+", "")
+    if version == "" then
+        error("Failed to get Vulkan latest version")
+    end
+    return version
+end
+
+local function vulkan_sdk_installed(version)
+    local path = VulkanSDKDir .. "/" .. version
+    local ok = os.rename(path, path)
+    return ok ~= nil
+end
+
+local function clear_vulkan_sdk_dir()
+    if package.config:sub(1,1) == '\\' then
+        os.execute('rmdir /S /Q "' .. VulkanSDKDir .. '"')
+    else
+        os.execute('rm -rf "' .. VulkanSDKDir .. '"')
+    end
+end
+
+local function install_vulkan_sdk(version)
+    local sdk_url = VulkanSDKBaseURL .. version .. "/windows/VulkanSDK-" .. version .. "-Installer.exe"
+    local installer = VulkanSDKDir .. "/VulkanSDKInstaller.exe"
+    os.execute('mkdir "' .. VulkanSDKDir .. '"')
+    os.execute('curl -L -o "' .. installer .. '" "' .. sdk_url .. '"')
+    os.execute('"' .. installer .. '" /S /D=' .. VulkanSDKDir .. '/' .. version)
+end
+
+local VulkanLatestVersion = get_latest_vulkan_version()
+if not vulkan_sdk_installed(VulkanLatestVersion) then
+    clear_vulkan_sdk_dir()
+    install_vulkan_sdk(VulkanLatestVersion)
+end
+
+VulkanSDKVersion = VulkanLatestVersion
 
 include "vendor/GLFW/Build-GLFW.lua"
 
@@ -27,7 +74,7 @@ project "Axiom"
     IncludeDir = {}
     IncludeDir["glm"] = "vendor/glm"
     IncludeDir["glfw"] = "vendor/GLFW/include"
-    IncludeDir["vulkan"] = "vendor/VulkanSDK/Include"
+    IncludeDir["vulkan"] = "vendor/VulkanSDK/%{VulkanSDKVersion}/Include"
 
     includedirs {
         "src",
